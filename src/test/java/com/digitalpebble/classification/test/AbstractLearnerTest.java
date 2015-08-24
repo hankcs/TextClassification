e35d15bb53b17a395f0e716678dab591cf59a631
hankcs <jfservice@126.com>
2015-08-24 20:29:19 +0800

    补全语料库

--- src/test/java/com/digitalpebble/classification/test/AbstractLearnerTest.java
+++ src/test/java/com/digitalpebble/classification/test/AbstractLearnerTest.java
@@ -6,41 +6,46 @@ import junit.framework.TestCase;
 
 import com.digitalpebble.classification.Learner;
 
-public abstract class AbstractLearnerTest extends TestCase {
+public abstract class AbstractLearnerTest extends TestCase
+{
 
-	Learner learner;
-	File tempFile;
+    Learner learner;
+    File tempFile;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		// build a Learner
-		File tempFile2 = java.io.File.createTempFile("TextClassifier", "");
-		tempFile2.delete();
-		tempFile = new File(tempFile2.getParentFile(), "TextClassifierDir");
-		tempFile.mkdir();
-		learner = Learner.getLearner(tempFile.getAbsolutePath(),
-				Learner.LibSVMModelCreator, true);
-	}
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        // build a Learner
+        File tempFile2 = java.io.File.createTempFile("TextClassifier", "");
+        tempFile2.delete();
+        tempFile = new File(tempFile2.getParentFile(), "TextClassifierDir");
+        tempFile.mkdir();
+        learner = Learner.getLearner(tempFile.getAbsolutePath(),
+                                     Learner.LibSVMModelCreator, true);
+    }
 
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		removeDirectory(tempFile);
-		learner = null;
-	}
+    @Override
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+        removeDirectory(tempFile);
+        learner = null;
+    }
 
-	private static void removeDirectory(File directory) {
-		if (directory.isDirectory() == false)
-			return;
-		File[] content = directory.listFiles();
-		for (int i = 0; i < content.length; i++) {
-			if (content[i].isDirectory())
-				removeDirectory(content[i]);
-			else
-				content[i].delete();
-		}
-		directory.delete();
-	}
+    private static void removeDirectory(File directory)
+    {
+        if (directory.isDirectory() == false)
+            return;
+        File[] content = directory.listFiles();
+        for (int i = 0; i < content.length; i++)
+        {
+            if (content[i].isDirectory())
+                removeDirectory(content[i]);
+            else
+                content[i].delete();
+        }
+        directory.delete();
+    }
 
 }
